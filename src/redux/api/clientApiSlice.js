5feb3a4cd6749f49f9caff0c69d0f94ab2066966
khushbuu-07 <khushbuusaifi09@gmail.com>
2026-02-25 11:16:35 +0530

    clientquery apis integarted

--- src/redux/api/clientApiSlice.js
+++ src/redux/api/clientApiSlice.js
@@ -1,6 +1,7 @@
 import { apiSlice } from './apiSlice';
 
 const CLIENTS_URL = 'clients';
+const FORMS_URL = 'forms';
 
 export const clientApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
@@ -34,6 +35,77 @@ export const clientApiSlice = apiSlice.injectEndpoints({
       transformResponse: (response) => response.data,
       providesTags: (result, error, arg) => [
         { type: 'Client', id: arg },
+      ],
+    }),
+
+    getForms: builder.query({
+      query: ({ page = 1, limit = 10, search = '', status = '' } = {}) => ({
+        url: FORMS_URL,
+        params: { page, limit, search, status },
+      }),
+      transformResponse: (response) => response,
+      providesTags: (result) =>
+        Array.isArray(result?.data)
+          ? [
+              { type: 'Form', id: 'LIST' },
+              ...result.data.map((form) => ({
+                type: 'Form',
+                id: form._id,
+              })),
+            ]
+          : [{ type: 'Form', id: 'LIST' }],
+      keepUnusedDataFor: 5,
+    }),
+
+    getFormByFormId: builder.query({
+      query: (formId) => `${FORMS_URL}/${formId}`,
+      transformResponse: (response) => response.data,
+      providesTags: (result, error, arg) => [
+        { type: 'Form', id: result?._id || arg },
+      ],
+    }),
+
+    submitForm: builder.mutation({
+      query: (data) => ({
+        url: `${FORMS_URL}/submit`,
+        method: 'POST',
+        body: data,
+      }),
+      invalidatesTags: [{ type: 'Form', id: 'LIST' }],
+    }),
+
+    updateFormStatus: builder.mutation({
+      query: ({ id, status }) => ({
+        url: `${FORMS_URL}/${id}/status`,
+        method: 'PATCH',
+        body: { status },
+      }),
+      invalidatesTags: (result, error, arg) => [
+        { type: 'Form', id: 'LIST' },
+        { type: 'Form', id: arg.id },
+      ],
+    }),
+
+    assignForm: builder.mutation({
+      query: ({ id, userId }) => ({
+        url: `${FORMS_URL}/${id}/assign`,
+        method: 'PATCH',
+        body: { userId },
+      }),
+      invalidatesTags: (result, error, arg) => [
+        { type: 'Form', id: 'LIST' },
+        { type: 'Form', id: arg.id },
+      ],
+    }),
+
+    deleteForm: builder.mutation({
+      query: (id) => ({
+        url: `${FORMS_URL}/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: (result, error, arg) => [
+        { type: 'Form', id: 'LIST' },
+        { type: 'Form', id: arg },
       ],
     }),
 
@@ -76,6 +148,13 @@ export const clientApiSlice = apiSlice.injectEndpoints({
 export const {
   useGetClientsQuery,
   useGetClientQuery,
+  useGetFormsQuery,
+  useGetFormByFormIdQuery,
+  useLazyGetFormByFormIdQuery,
+  useSubmitFormMutation,
+  useUpdateFormStatusMutation,
+  useAssignFormMutation,
+  useDeleteFormMutation,
   useAddClientMutation,
   useUpdateClientMutation,
   useDeleteClientMutation,
